cb1d877ac8ecac7dcd224d264c50ac6992d207a8
ananyashetty-bits <f20240748@goa.bits-pilani.ac.in>
2026-05-28 01:55:38 +0530

    Fixed CORS for deployment

--- backend/main.py
+++ backend/main.py
@@ -20,10 +20,7 @@ app = FastAPI()
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-    "http://localhost:5173",
-    "http://localhost:5174",
-],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
