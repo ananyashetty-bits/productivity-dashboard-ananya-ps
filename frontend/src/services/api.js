712c876a579a66c72a37c94b3e04a5617245a10f
ananyashetty-bits <f20240748@goa.bits-pilani.ac.in>
2026-05-28 01:32:58 +0530

    Connected frontend to deployed backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "http://127.0.0.1:8000"="https://productivity-dashboard-ananya-ps.onrender.com";
 
 // ---------------- TASKS ----------------
 
